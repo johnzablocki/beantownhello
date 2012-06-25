a0eb42fe65b3ad7e30e663bb08892a59be03197b
johnzablocki <dev@zblock.net>
2012-06-25 19:39:06 -0400

    Fixed port issue

--- web.js
+++ web.js
@@ -5,4 +5,4 @@ var message = { "message" : "Hello, World!" };
 http.createServer(function(req, res) {
     res.writeHeader(200, { "Content-Type" : "application/json" });
     res.end(JSON.stringify(message));
-}).listen(8675, "0.0.0.0");
+}).listen(process.env.PORT || 8675, "0.0.0.0");
